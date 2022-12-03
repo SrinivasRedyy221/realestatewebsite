debedf0bfb24c2aaee0eab312e66c1fb150e4bdc
SrinivasRedyy221 <76741223+SrinivasRedyy221@users.noreply.github.com>
2022-12-03 16:53:03 +0530

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About() {
         </div>
         <div className='about-text'>
             <h2>We are the Best<br/>Reat estate Company</h2>
-            <p>if you search around the hyderabad also you would only find out that the prices and responce of our agents is better and efficent</p>
+            <p>if you search in hyderabad also you would only find out that the prices and responce of our agents is better and efficent</p>
             <button>View more details</button>
         </div>
       
