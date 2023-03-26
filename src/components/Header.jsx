7dc7b8632f2ee91fef24acbba119aef34a8d0b5f
SrinivasRedyy221 <76741223+SrinivasRedyy221@users.noreply.github.com>
2023-03-26 09:41:04 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <div className='header'>
       <Navbar/>
       <div className='intro'>
-        <p>Looking for a Property!</p>
+        <p>Looking fr a Property!</p>
         <h1><span>Buy</span> and <span>Sell</span></h1>
         <p className='details'>hello there here is a great thing to look for and a great place to buy and sell properties</p>
         <a href="#" className='header-btn'>Details</a>
